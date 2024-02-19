fc1d70a2d35815a7bcec5cbabde0fe3c86807dc0
herbertfilhik <herbertfilhik@gmail.com>
2024-02-19 03:02:08 -0300

    comment updated

--- Tests/ComponentTests/StudyGroupControllerTests.cs
+++ Tests/ComponentTests/StudyGroupControllerTests.cs
@@ -1,90 +1,110 @@
-﻿using Moq;
-using StudyGroupsManager.Models;
-using Microsoft.AspNetCore.Mvc;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using Moq; // Mocking library for creating mock objects
+using StudyGroupsManager.Models; // Contains the models used in the application
+using Microsoft.AspNetCore.Mvc; // ASP.NET Core MVC framework for handling web requests
+using NUnit.Framework; // NUnit framework for unit testing
+using System; // System namespace
+using System.Collections.Generic; // Namespace for working with collections
+using System.Threading.Tasks; // Namespace for working with asynchronous tasks
 
 namespace StudyGroupsManager.Tests.ComponentTests
 {
     [TestFixture]
     public class StudyGroupControllerTests
     {
-        private Mock<IStudyGroupRepository> _mockRepository;
-        private StudyGroupController _controller;
+        private Mock<IStudyGroupRepository> _mockRepository; // Mock repository for testing
+        private StudyGroupController _controller; // Controller being tested
 
         [SetUp]
         public void Setup()
         {
-            _mockRepository = new Mock<IStudyGroupRepository>();
-            _controller = new StudyGroupController(_mockRepository.Object);
+            _mockRepository = new Mock<IStudyGroupRepository>(); // Initializing mock repository
+            _controller = new StudyGroupController(_mockRepository.Object); // Initializing controller with mock repository
         }
 
+        // Test case to ensure CreateStudyGroup action returns OkResult when called
         [Test]
         public async Task CreateStudyGroup_WhenCalled_ShouldReturnOk()
         {
-            var studyGroup = new StudyGroup { Name = "Math Study Group", Subject = Subject.Math };
-            _mockRepository.Setup(repo => repo.CreateStudyGroup(It.IsAny<StudyGroup>()))
-                           .Returns(Task.CompletedTask);
+            // Arrange
+            var studyGroup = new StudyGroup { Name = "Math Study Group", Subject = Subject.Math }; // Creating a study group object
+            _mockRepository.Setup(repo => repo.CreateStudyGroup(It.IsAny<StudyGroup>())) // Setting up mock repository behavior
+                           .Returns(Task.CompletedTask); // Mocking the behavior to return a completed task
 
-            var result = await _controller.CreateStudyGroup(studyGroup);
+            // Act
+            var result = await _controller.CreateStudyGroup(studyGroup); // Invoking the action method
 
-            Assert.IsInstanceOf<OkResult>(result);
+            // Assert
+            Assert.IsInstanceOf<OkResult>(result); // Verifying if the result is of type OkResult
         }
 
+        // Test case to ensure GetStudyGroups action returns all study groups
         [Test]
         public async Task GetStudyGroups_WhenCalled_ShouldReturnAllStudyGroups()
         {
-            var studyGroups = new List<StudyGroup> { new StudyGroup(1, "Math Group", Subject.Math, DateTime.Now, new List<User>()) };
-            _mockRepository.Setup(repo => repo.GetStudyGroups()).ReturnsAsync(studyGroups);
+            // Arrange
+            var studyGroups = new List<StudyGroup> { new StudyGroup(1, "Math Group", Subject.Math, DateTime.Now, new List<User>()) }; // Creating a list of study groups
+            _mockRepository.Setup(repo => repo.GetStudyGroups()).ReturnsAsync(studyGroups); // Setting up mock repository behavior to return study groups
 
-            var result = await _controller.GetStudyGroups();
+            // Act
+            var result = await _controller.GetStudyGroups(); // Invoking the action method
 
-            var objectResult = result as OkObjectResult;
-            Assert.IsNotNull(objectResult);
-            var model = objectResult.Value as IEnumerable<StudyGroup>;
-            Assert.IsNotNull(model);
-            Assert.That(model, Is.EquivalentTo(studyGroups));
+            // Assert
+            var objectResult = result as OkObjectResult; // Converting the result to OkObjectResult
+            Assert.IsNotNull(objectResult); // Verifying if the result is not null
+            var model = objectResult.Value as IEnumerable<StudyGroup>; // Extracting the value from the result
+            Assert.IsNotNull(model); // Verifying if the model is not null
+            Assert.That(model, Is.EquivalentTo(studyGroups)); // Verifying if the returned study groups are equivalent to the expected study groups
         }
 
+        // Test case to ensure JoinStudyGroup action returns OkResult with valid data
         [Test]
         public async Task JoinStudyGroup_WithValidData_ShouldReturnOk()
         {
-            _mockRepository.Setup(repo => repo.JoinStudyGroup(It.IsAny<int>(), It.IsAny<int>()))
-                           .Returns(Task.CompletedTask);
+            // Arrange
+            _mockRepository.Setup(repo => repo.JoinStudyGroup(It.IsAny<int>(), It.IsAny<int>())) // Setting up mock repository behavior
+                           .Returns(Task.CompletedTask); // Mocking the behavior to return a completed task
 
-            var result = await _controller.JoinStudyGroup(1, 1);
+            // Act
+            var result = await _controller.JoinStudyGroup(1, 1); // Invoking the action method
 
-            Assert.IsInstanceOf<OkResult>(result);
+            // Assert
+            Assert.IsInstanceOf<OkResult>(result); // Verifying if the result is of type OkResult
         }
 
+        // Test case to ensure LeaveStudyGroup action returns OkResult with valid data
         [Test]
         public async Task LeaveStudyGroup_WithValidData_ShouldReturnOk()
         {
-            _mockRepository.Setup(repo => repo.LeaveStudyGroup(It.IsAny<int>(), It.IsAny<int>()))
-                           .Returns(Task.CompletedTask);
+            // Arrange
+            _mockRepository.Setup(repo => repo.LeaveStudyGroup(It.IsAny<int>(), It.IsAny<int>())) // Setting up mock repository behavior
+                           .Returns(Task.CompletedTask); // Mocking the behavior to return a completed task
 
-            var result = await _controller.LeaveStudyGroup(1, 1);
+            // Act
+            var result = await _controller.LeaveStudyGroup(1, 1); // Invoking the action method
 
-            Assert.IsInstanceOf<OkResult>(result);
+            // Assert
+            Assert.IsInstanceOf<OkResult>(result); // Verifying if the result is of type OkResult
         }
 
+        // Test case to ensure SearchStudyGroups action returns filtered study groups based on subject
         [Test]
         public async Task SearchStudyGroups_WithSubject_ShouldReturnFilteredStudyGroups()
         {
-            var subject = Subject.Math;
-            var filteredStudyGroups = new List<StudyGroup> { new StudyGroup(1, "Math Group", subject, DateTime.Now, new List<User>()) };
-            _mockRepository.Setup(repo => repo.SearchStudyGroups(subject.ToString()))
+            // Arrange
+            var subject = Subject.Math; // Setting up the subject for filtering
+            var filteredStudyGroups = new List<StudyGroup> { new StudyGroup(1, "Math Group", subject, DateTime.Now, new List<User>()) }; // Creating a list of filtered study groups
+            _mockRepository.Setup(repo => repo.SearchStudyGroups(subject.ToString())) // Setting up mock repository behavior to return filtered study groups
                            .ReturnsAsync(filteredStudyGroups);
 
-            var result = await _controller.SearchStudyGroups(subject.ToString());
+            // Act
+            var result = await _controller.SearchStudyGroups(subject.ToString()); // Invoking the action method
 
-            var objectResult = result as OkObjectResult;
-            Assert.IsNotNull(objectResult);
-            var returnedGroups = objectResult.Value as IEnumerable<StudyGroup>;
-            Assert.IsNotNull(returnedGroups);
-            Assert.That(returnedGroups, Is.EquivalentTo(filteredStudyGroups));
+            // Assert
+            var objectResult = result as OkObjectResult; // Converting the result to OkObjectResult
+            Assert.IsNotNull(objectResult); // Verifying if the result is not null
+            var returnedGroups = objectResult.Value as IEnumerable<StudyGroup>; // Extracting the value from the result
+            Assert.IsNotNull(returnedGroups); // Verifying if the returned groups are not null
+            Assert.That(returnedGroups, Is.EquivalentTo(filteredStudyGroups)); // Verifying if the returned study groups are equivalent to the filtered study groups
         }
 
         // Include new tests if necessary
--- Tests/UnitTests/StudyGroupTests.cs
+++ Tests/UnitTests/StudyGroupTests.cs
@@ -1,111 +1,112 @@
-﻿using StudyGroupsManager.Models;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
+﻿// Unit tests for the StudyGroup class
+using StudyGroupsManager.Models; // Importing necessary namespaces
+using NUnit.Framework; // Importing NUnit framework for testing
+using System; // Importing System namespace for basic functionalities
+using System.Collections.Generic; // Importing System.Collections.Generic for using Lists
 
-namespace StudyGroupsManager.Tests.UnitTests
+namespace StudyGroupsManager.Tests.UnitTests // Defining the namespace for unit tests
 {
-    [TestFixture]
-    public class StudyGroupTests
+    [TestFixture] // Indicates that this class contains tests
+    public class StudyGroupTests // Class to contain unit tests for StudyGroup
     {
-        [Test]
-        public void AddUser_WhenCalled_ShouldAddUserToStudyGroup()
+        [Test] // Indicates a test method
+        public void AddUser_WhenCalled_ShouldAddUserToStudyGroup() // Test method to verify the AddUser functionality
         {
             // Arrange
-            var studyGroup = new StudyGroup(1, "Math Group", Subject.Math, DateTime.Now, new List<User>());
-            var user = new User { Id = 1, Name = "Maria" };
+            var studyGroup = new StudyGroup(1, "Math Group", Subject.Math, DateTime.Now, new List<User>()); // Creating a study group
+            var user = new User { Id = 1, Name = "Maria" }; // Creating a user to be added to the study group
 
             // Act
-            studyGroup.AddUser(user);
+            studyGroup.AddUser(user); // Adding the user to the study group
 
             // Assert
-            Assert.Contains(user, studyGroup.Users);
+            Assert.Contains(user, studyGroup.Users); // Verifying if the user has been added to the study group
         }
 
-        [Test]
-        public void RemoveUser_WhenCalled_ShouldRemoveUserFromStudyGroup()
+        [Test] // Another test method
+        public void RemoveUser_WhenCalled_ShouldRemoveUserFromStudyGroup() // Test method to verify the RemoveUser functionality
         {
             // Arrange
-            var user = new User { Id = 1, Name = "Maria" };
-            var studyGroup = new StudyGroup(1, "Math Group", Subject.Math, DateTime.Now, new List<User> { user });
+            var user = new User { Id = 1, Name = "Maria" }; // Creating a user
+            var studyGroup = new StudyGroup(1, "Math Group", Subject.Math, DateTime.Now, new List<User> { user }); // Creating a study group and adding the user to it
 
             // Act
-            studyGroup.RemoveUser(user);
+            studyGroup.RemoveUser(user); // Removing the user from the study group
 
             // Assert
-            Assert.IsFalse(studyGroup.Users.Contains(user));
+            Assert.IsFalse(studyGroup.Users.Contains(user)); // Verifying if the user has been removed from the study group
         }
 
-        [Test]
-        public void CreateStudyGroup_WithInvalidNameLength_ShouldThrowArgumentException()
+        [Test] // Another test method
+        public void CreateStudyGroup_WithInvalidNameLength_ShouldThrowArgumentException() // Test method to verify the behavior when creating a study group with an invalid name length
         {
             // Arrange
-            var nameTooShort = "Math";
-            var nameTooLong = new string('A', 31); // Example of a 31-character name.
+            var nameTooShort = "Math"; // Setting a name that is too short
+            var nameTooLong = new string('A', 31); // Setting a name that is too long
 
             // Act & Assert
-            Assert.Throws<ArgumentException>(() => new StudyGroup(2, nameTooShort, Subject.Math, DateTime.Now, new List<User>()));
-            Assert.Throws<ArgumentException>(() => new StudyGroup(3, nameTooLong, Subject.Math, DateTime.Now, new List<User>()));
+            Assert.Throws<ArgumentException>(() => new StudyGroup(2, nameTooShort, Subject.Math, DateTime.Now, new List<User>())); // Verifying if ArgumentException is thrown for a name that is too short
+            Assert.Throws<ArgumentException>(() => new StudyGroup(3, nameTooLong, Subject.Math, DateTime.Now, new List<User>())); // Verifying if ArgumentException is thrown for a name that is too long
         }
 
-        [Test]
-        public void CreateStudyGroup_WithInvalidSubject_ShouldThrowArgumentException()
+        [Test] // Another test method
+        public void CreateStudyGroup_WithInvalidSubject_ShouldThrowArgumentException() // Test method to verify the behavior when creating a study group with an invalid subject
         {
             // Arrange
-            var invalidSubject = (Subject)999; // Example of an invalid ENUM value.
+            var invalidSubject = (Subject)999; // Setting an invalid subject value
 
             // Act & Assert
-            Assert.Throws<ArgumentException>(() => new StudyGroup(4, "Invalid Study Group", invalidSubject, DateTime.Now, new List<User>()));
+            Assert.Throws<ArgumentException>(() => new StudyGroup(4, "Invalid Study Group", invalidSubject, DateTime.Now, new List<User>())); // Verifying if ArgumentException is thrown for an invalid subject
         }
 
-        [Test]
-        public void CreateStudyGroup_WithNameWithinValidRange_ShouldSucceed()
+        [Test] // Another test method
+        public void CreateStudyGroup_WithNameWithinValidRange_ShouldSucceed() // Test method to verify the behavior when creating a study group with a valid name
         {
             // Arrange
-            var validName = "Valid Group";
-            var studyGroup = new StudyGroup(1, validName, Subject.Math, DateTime.Now, new List<User>());
+            var validName = "Valid Group"; // Setting a valid name
 
             // Act & Assert
-            Assert.That(studyGroup.Name, Is.EqualTo(validName));
+            var studyGroup = new StudyGroup(1, validName, Subject.Math, DateTime.Now, new List<User>()); // Creating a study group with a valid name
+            Assert.That(studyGroup.Name, Is.EqualTo(validName)); // Verifying if the study group has been created with the expected name
         }
 
-        [Test]
-        public void CreateStudyGroup_WithValidSubject_ShouldSucceed()
+        [Test] // Another test method
+        public void CreateStudyGroup_WithValidSubject_ShouldSucceed() // Test method to verify the behavior when creating a study group with a valid subject
         {
             // Arrange
-            var validSubject = Subject.Chemistry;
-            var studyGroup = new StudyGroup(1, "Chemistry Group", validSubject, DateTime.Now, new List<User>());
+            var validSubject = Subject.Chemistry; // Setting a valid subject
 
             // Act & Assert
-            Assert.That(studyGroup.Subject, Is.EqualTo(validSubject));
+            var studyGroup = new StudyGroup(1, "Chemistry Group", validSubject, DateTime.Now, new List<User>()); // Creating a study group with a valid subject
+            Assert.That(studyGroup.Subject, Is.EqualTo(validSubject)); // Verifying if the study group has been created with the expected subject
         }
 
-        [Test]
-        public void CreateStudyGroup_WithCreationDate_ShouldRecordCreationDate()
+        [Test] // Another test method
+        public void CreateStudyGroup_WithCreationDate_ShouldRecordCreationDate() // Test method to verify if the creation date is recorded when creating a study group
         {
             // Arrange
-            var creationDate = DateTime.Now;
-            var studyGroup = new StudyGroup(1, "Physics Group", Subject.Physics, creationDate, new List<User>());
+            var creationDate = DateTime.Now; // Setting the current date as the creation date
 
             // Act & Assert
-            Assert.That(studyGroup.CreateDate, Is.EqualTo(creationDate));
+            var studyGroup = new StudyGroup(1, "Physics Group", Subject.Physics, creationDate, new List<User>()); // Creating a study group with a specified creation date
+            Assert.That(studyGroup.CreateDate, Is.EqualTo(creationDate)); // Verifying if the study group has been created with the expected creation date
         }
 
-        [Test]
-        public void User_CanJoinMultipleStudyGroupsOfDifferentSubjects()
+        [Test] // Another test method
+        public void User_CanJoinMultipleStudyGroupsOfDifferentSubjects() // Test method to verify if a user can join multiple study groups of different subjects
         {
             // Arrange
-            var user = new User { Id = 1, Name = "João" };
-            var mathGroup = new StudyGroup(1, "Math Group", Subject.Math, DateTime.Now, new List<User>());
-            var chemistryGroup = new StudyGroup(2, "Chemistry Group", Subject.Chemistry, DateTime.Now, new List<User>());
+            var user = new User { Id = 1, Name = "João" }; // Creating a user
+            var mathGroup = new StudyGroup(1, "Math Group", Subject.Math, DateTime.Now, new List<User>()); // Creating a math study group
+            var chemistryGroup = new StudyGroup(2, "Chemistry Group", Subject.Chemistry, DateTime.Now, new List<User>()); // Creating a chemistry study group
 
             // Act
-            mathGroup.AddUser(user);
-            chemistryGroup.AddUser(user);
+            mathGroup.AddUser(user); // Adding the user to the math study group
+            chemistryGroup.AddUser(user); // Adding the user to the chemistry study group
 
             // Assert
-            Assert.IsTrue(mathGroup.Users.Contains(user), "User should be in the math group.");
-            Assert.IsTrue(chemistryGroup.Users.Contains(user), "User should be in the chemistry group.");
+            Assert.IsTrue(mathGroup.Users.Contains(user), "User should be in the math group."); // Verifying if the user is in the math study group
+            Assert.IsTrue(chemistryGroup.Users.Contains(user), "User should be in the chemistry group."); // Verifying if the user is in the chemistry study group
         }
     }
 }
